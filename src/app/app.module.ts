bede40b2530478154511c3e1e0c81b502151e309
naruto <naruto_1218@hotmail.com>
2017-10-18 15:41:17 +0300

    Add Button Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { MatInputModule } from '@angular/material';
 import { HttpModule }    from '@angular/http';
 import { MatFormFieldModule } from '@angular/material';
 import { MatTableModule } from '@angular/material';
+import { MatButtonModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
@@ -20,7 +21,8 @@ import { AppComponent } from './app.component';
     MatInputModule,
     BrowserAnimationsModule,
     MatFormFieldModule,
-    MatTableModule
+    MatTableModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
